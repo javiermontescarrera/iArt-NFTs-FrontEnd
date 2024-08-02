19dc97e10a370145f35cc103d5a5dd154a893aaa
Javier Montes <javier.montes.carrera@gmail.com>
2024-08-02 16:28:50 -0500

    refactor: Cleanup image genearation

--- app/api/images/route.ts
+++ app/api/images/route.ts
@@ -3,7 +3,7 @@ import { HfInference } from "@huggingface/inference";
 const hf = new HfInference(process.env.HUGGING_FACE_API_KEY);
 
 export const runtime = "edge";
-const URL = `https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2-1`;
+// const URL = `https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2-1`;
 const model = 'stabilityai/stable-diffusion-2-1';
 // const model = 'stabilityai/stable-diffusion-2';
 // const model = 'runwayml/stable-diffusion-v1-5';
@@ -12,14 +12,15 @@ const model = 'stabilityai/stable-diffusion-2-1';
 
 export async function POST(req: Request) {
   const { message } = await req.json();
-  const prompt = `You are a very professional and versatile painter who can create any style of painting. You have to generate an painting following gracefully these instructions: ${message}`;
-  const inputs = prompt.substring(0, Math.min(prompt.length, 5000));
+  const prompt = `You are a very professional and skilled painter who can create any style of painting. You have to generate an painting following gracefully these instructions: ${message}`;
+  // const inputs = prompt.substring(0, Math.min(prompt.length, 5000));
 
   // console.log(inputs);
 
   try {
     const response = await hf.textToImage({
-      inputs: inputs,
+      // inputs: inputs,
+      inputs: prompt,
       model: model,
       parameters: {
         negative_prompt: 'blurry',
